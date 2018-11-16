508f150783537672a099637a7e1a550959a9e139
KamLan <adouardcamille@hotmail.com>
2018-11-16 15:24:57 +0100

    add chartjs and statistic method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
+    <Header/>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
+    <Statistics/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './components/Home.vue'
+import Header from './components/Header.vue'
+import Statistics from './components/Statistics.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home,
+    Header,
+    Statistics
   }
 }
 </script>
@@ -23,6 +28,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
